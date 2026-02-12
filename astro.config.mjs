8142a4ef783a4e616f8971220f060c320078c7a4
HoussemKamas <houssemkamas96@gmail.com>
2026-02-12 14:08:17 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://houssemkamas.github.io', // عنوان GitHub Pages الأساسي لحسابك
+  base: '/domix',                          // اسم الريبو مع سلاش في البداية
+  output: 'static',
+});
